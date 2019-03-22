343956af1e442228cc15ae675cf5f1e40ded139c
Jamie Boyd <enkizero@gmail.com>
2019-03-22 11:55:35 +0000

    Updated imports and exports in index.js for more granular API

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 //  Good Utilities - index.js
 
+/////////////
+// Imports //
+/////////////
+
+import collapseElement from "./DOM/collapseElement";
+import createNodeFromHTML from "./DOM/createNodeFromHTML";
+import expandElement from "./DOM/expandElement";
+import getClosestParent from "./DOM/getClosestParent";
 import getIndexOfNode from "./DOM/getIndexOfNode";
+import getOffset from "./DOM/getOffset";
+import getOuterHeight from "./DOM/getOuterHeight";
+import getOuterWidth from "./DOM/getOuterWidth";
+import getURLQueryString from "./DOM/getURLQueryString";
+import isElementInView from "./DOM/isElementInView";
 import ready from "./DOM/ready";
+import removeInlineStyles from "./DOM/removeInlineStyles";
+import wrapElement from "./DOM/wrapElement";
 
+/////////////
+// Exports //
+/////////////
 
-export default {
-  getIndexOfNode: getIndexOfNode,
-  ready: ready
-}
+export { collapseElement };
+export { createNodeFromHTML };
+export { expandElement };
+export { getClosestParent };
+export { getIndexOfNode };
+export { getOffset };
+export { getOuterHeight };
+export { getOuterWidth };
+export { getURLQueryString };
+export { isElementInView };
+export { ready };
+export { removeInlineStyles };
+export { wrapElement };
