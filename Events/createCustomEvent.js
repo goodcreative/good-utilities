c7a5738ef50da4298d54533837d5e8ac389414d0
Jamie Boyd <enkizero@gmail.com>
2019-05-20 16:04:15 +0100

    Updated createCustomEvent to fixed another data issue

--- Events/createCustomEvent.js
+++ Events/createCustomEvent.js
@@ -9,15 +9,11 @@
 export default function createCustomEvent(eventName, eventData) {
   let customEvent;
 
-  if(eventData === undefined) {
-    eventData = "no data";
-  }
-
   if (window.CustomEvent) {
-    customEvent = new CustomEvent(eventName, { payload: eventData, bubbles: true });
+    customEvent = new CustomEvent(eventName, { detail: { data: eventData }, bubbles: true });
   } else {
     customEvent = document.createEvent('CustomEvent');
-    customEvent.initCustomEvent(eventName, true, true, { payload: eventData });
+    customEvent.initCustomEvent(eventName, true, true, { data: eventData });
   }
 
   return customEvent;
